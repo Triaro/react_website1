61cfce87f8eb9ff4c7ab72d752761367766a9eec
Triaro <abhimehra2762@gmail.com>
2021-01-19 19:49:11 +0530

    commit 2

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -2,11 +2,12 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 function CardItem(props) {
     return (
-        <>
+        <>       
         <li class="cards__item">
             <Link className="cards__item__link" to={props.path}>
                 <figure class="cards__item__pic-wrap" data-category={props.label}>
-                    <img src={props.src} alt="Travel Image" class="cards__item__img"/>
+                 {/* eslint-disable-next-line */}
+                <img src={props.src} alt="Travel Image" class="cards__item__img"/>
                 </figure>
                 <div class="cards__item__info">
                     <h5 class="cards__item__text">{props.text}</h5>
